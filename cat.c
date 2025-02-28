eec669c78a7453ba20e02e1a342d1afed4b6d83a
MrFish486 <laszlodeanpav@gmail.com>
2025-02-27 17:47:41 -0800

    Added some coreutils

---
+++ cat.c
@@ -0,0 +1,22 @@
+//Compile with "gcc -o cat cat.c"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+int main(int argc, char **argv){
+	if(argc>1){
+		for(int i=1;i<argc;i++){
+			FILE* h=fopen(argv[i],"r");
+			char c;
+			while((c=fgetc(h))!=EOF){
+				printf("%c",c);
+			}
+			fclose(h);
+		}
+	}else{
+		while(0==0){ // Didn't want <stdbool.h>
+			char in[1];
+			scanf("%c",&in);
+			printf(in);
+		}
+	}
+}
---
+++ ls.c
@@ -0,0 +1,83 @@
+//Compile with "gcc -o ls ls.c -lm"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <dirent.h>
+#include <math.h>
+int main(int argc, char **argv){
+	if(argc!=1){
+		for(int i=1;i<argc;i++){
+			struct dirent *de;
+			DIR *dr = opendir(argv[i]);
+			if(dr == NULL){
+				printf("\033[1;31mError; can't open folder \033[1;33m%s\033[0;38m\n",argv[i]);
+				return 1;
+			}else{
+				while((de=readdir(dr))!=NULL){
+					if(de->d_type==DT_DIR){
+						int d=0;
+						d=(de->d_ino==0)?1:log10(de->d_ino)+1;
+						printf("Inode:%d",de->d_ino);
+						for(int q=0;q<10-d;q++){
+							printf(" ");
+						}
+						printf("Type:Dir       \033[1;33m%s\033[0;38m",de->d_name);
+					}else if(de->d_type==DT_REG){
+						int d=0;
+						d=(de->d_ino==0)?1:log10(de->d_ino)+1;
+						printf("Inode:%d",de->d_ino);
+						for(int q=0;q<10-d;q++){
+							printf(" ");
+						}
+						printf("Type:File      \033[1;32m%s\033[0;38m",de->d_name);
+					}else if(de->d_type==DT_LNK){
+						int d=0;
+						d=(de->d_ino==0)?1:log10(de->d_ino)+1;
+						printf("Inode:%d",de->d_ino);
+						for(int q=0;q<10-d;q++){
+							printf(" ");
+						}
+						printf("Type:Link      \033[1;31m%s\033[0;38m",de->d_name);
+					}
+					printf("\n");
+				}
+			}
+		}
+	}else{
+                struct dirent *de;
+                DIR *dr = opendir(".");
+                if(dr == NULL){
+                        printf("\033[1;31mError; can't open folder \033[1;33m.\033[0;38m\n");
+                        return 1;
+                }else{
+                        while((de=readdir(dr))!=NULL){
+                                if(de->d_type==DT_DIR){
+                                        int d=0;
+                                        d=(de->d_ino==0)?1:log10(de->d_ino)+1;
+                                        printf("Inode:%d",de->d_ino);
+                                        for(int q=0;q<10-d;q++){
+                                                printf(" ");
+                                        }
+                                        printf("Type:Dir       \033[1;33m%s\033[0;38m",de->d_name);
+                                }else if(de->d_type==DT_REG){
+                                        int d=0;
+                                        d=(de->d_ino==0)?1:log10(de->d_ino)+1;
+                                        printf("Inode:%d",de->d_ino);
+                                        for(int q=0;q<10-d;q++){
+                                                printf(" ");
+                                        }
+                                        printf("Type:File      \033[1;32m%s\033[0;38m",de->d_name);
+                                }else if(de->d_type==DT_LNK){
+                                        int d=0;
+                                        d=(de->d_ino==0)?1:log10(de->d_ino)+1;
+                                        printf("Inode:%d",de->d_ino);
+                                        for(int q=0;q<10-d;q++){
+                                                printf(" ");
+                                        }
+                                        printf("Type:Link      \033[1;31m%s\033[0;38m",de->d_name);
+                                }
+                                printf("\n");
+                        }
+                }
+	}
+}
