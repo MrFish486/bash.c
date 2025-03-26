1af5b6f00f474b617bbd272724f719eb929c6c9f
MrFish486 <laszlodeanpav@gmail.com>
2025-03-26 15:53:48 -0700

    Added editor, still a WIP

---
+++ clear.c
@@ -0,0 +1,4 @@
+#include <stdio.h>
+int main(int argc, char **argv){
+	printf("\e[1;1H\e[2J");
+}
---
+++ editor.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+#include <stdlib.h>
+int main(int argc, char **argv){
+	if(argc == 1){
+		printf("No file!\n");
+		return 1;
+	}
+	// Reading
+	char * buf = 0;
+	FILE* o = fopen(argv[1], "r");
+	fseek(o, 0L, SEEK_END);
+	long long size = ftell(o);
+	long long int lines = 0;
+	char c;
+	rewind(o);
+	buf = malloc(size);
+	if(buf){
+		fread(buf, 1, size, o);
+	}
+	rewind(o);
+	while((c = fgetc(o)) != EOF){
+		if(c == '\n'){
+			lines++;
+		}
+	}
+	fclose(o);
+	// Reading / writing to terminal
+	struct winsize w;
+	ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
+	int ws = w.ws_row;
+	system("clear");
+	if(ws < lines){
+		printf("\e[1;33mFile doesn't fit on screen.\e[0;38m\n");
+	}else if(ws > lines || ws == lines){
+		printf("\e[1;32mFile fits on screen.\e[0;38m\n");
+	}
+	printf("Size  : %dB\nLines : %d\nEnter to continue.", size, lines);
+	getchar();
+	if(ws < lines){
+		// Split buf into lines
+		char linestore[lines][256];
+		long long int a = 0;
+		long long int b = 0;
+		while(1){
+			if(buf[b] == '\n'){
+				a++;
+			}
+			if(a == lines){
+				break;
+			}
+			linestore[a][b] = buf[b];
+		}
+		for(int i = 0; i < lines; i++){
+			
+		}
+	}
+	// Exit
+	free(buf);
+}
